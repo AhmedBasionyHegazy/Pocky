013d86f45ae14b16ee4d75a277d0c0a0241b39af
Ahmed Hegazy <Ahmed.Hegazy@technexus.ae>
2024-06-19 05:23:44 +0300

    start AuthenticationStateProvider implementation

---
+++ Pocky.WASM/Identity/CookieAuthenticationStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Pocky.WASM.Identity
+{
+    public class CookieAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private bool _autenticated;
+        private readonly ClaimsPrincipal _unauhenticated = new (new ClaimsIdentity());
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            _autenticated = false;
+
+            var user = _unauhenticated;
+
+            return new AuthenticationState(user);
+        }
+    }
+}
